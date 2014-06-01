a7346d0b165f000be516457e3df0264e4749befc
Junaos <Junaos@gmail.com>
2014-06-01 01:17:16 -0700

    Chapter 1, Exercises 1-4

---
+++ Circle/src/Circle.java
@@ -0,0 +1,18 @@
+
+class Circle {
+    public static void main(String[] args) {
+        double PI = 3.14159;
+        double radius = 15;
+        double diameter = radius * 2;
+        double circumference = PI * diameter;
+        double area = PI * (radius * radius);
+        System.out.println("The diameter of the circle is: " + diameter);
+        //System.out.println(diameter);
+        System.out.println("The circumference of the circle is: " + circumference);
+        //System.out.println(circumference);
+        System.out.println("The area of the circle is: " + area);
+        //System.out.println(area);
+        
+    }
+    
+}
---
+++ Hello World/src/Hello.java
@@ -0,0 +1,11 @@
+
+class Hello {
+    
+    // main: generate Hello World
+    
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+        System.out.println("How are you?");
+        
+    }
+}
